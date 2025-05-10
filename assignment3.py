013d3b9de269760a20f9773febdfa50739392001
YUVALGARG <youryuval.19@gmail.com>
2025-05-10 16:46:13 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,26 @@
+import math
+
+# Task 1: Calculate Factorial Using a Function 
+n = int(input("ENTER A NUMBER: "))
+def factorial(n):
+    if  n < 2 :
+        return(1)
+    else: 
+        return(n*factorial(n-1)) 
+
+factorial_result = factorial(n)
+print("Factorial of", n, "is:", factorial_result)
+
+# Task 2: Using the Math Module for Calculations
+import math
+
+number = float(input("Enter a number for math calculations: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+
+print(f"Square root of {number} is: {square_root}")
+print(f"Natural logarithm of {number} is: {natural_log}")
+print(f"Sine of {number} (in radians) is: {sine_value}")
+
